f83ff48e8282fd8e3423896491c0bbef823dd93c
김리안 <77778854+kimriany@users.noreply.github.com>
2021-07-13 14:44:56 +0900

    Add files via upload

---
+++ 새 폴더/main.js
@@ -0,0 +1,15 @@
+function a(){
+    var va = document.getElementById("net_worth")
+    
+    var v = parseInt(va.value);
+    console.log(v);
+    document.getElementById("main_h").innerText = v + "원 입금되었습니다";
+}
+
+document.getElementById("but").addEventListener("click",a 
+    
+    
+)
+
+
+

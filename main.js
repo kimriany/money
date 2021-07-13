96d5c7e56ce8b0124435aef97da118058d7e6155
김리안 <77778854+kimriany@users.noreply.github.com>
2021-07-13 15:13:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function a(){
     var vv = vvv.value
     var v = parseInt(va.value);
     console.log(v);
-    document.getElementById("main_h").innerText =vv + "에게 " + v + "원 입금되었습니다";
+    document.getElementById("main_h").innerText = vv + "에게 " + v + "원 입금되었습니다";
 }
 
 document.getElementById("but").addEventListener("click",a 
